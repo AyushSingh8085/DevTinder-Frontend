4be6682c5d755db759c0c4125a1d6c0948c3f1ca
Ayush Singh <Ayush.Singh@wallero.com>
2025-05-31 12:04:13 +0530

    feat: implement login functionality with axios for API calls

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div className="pb-2">
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label py-2">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                className="input input-bordered w-full max-x-xs"
+              />
+            </label>
+          </div>
+          <div className="pb-2">
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label py-2">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input input-bordered w-full max-x-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
