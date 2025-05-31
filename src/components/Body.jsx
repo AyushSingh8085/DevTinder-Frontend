713e570773f1b209d1ad5b30d243572d8f1da5df
Ayush Singh <Ayush.Singh@wallero.com>
2025-05-31 18:51:33 +0530

    implement user profile fetching and navigation in Body component; update Navbar links to use Link component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.error("Error fetching user profile:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!userData || !userData._id) {
+      // If user data is not available, fetch it
+      fetchUser();
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
